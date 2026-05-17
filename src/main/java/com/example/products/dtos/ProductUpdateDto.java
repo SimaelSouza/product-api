61d7f1e952f89eda80d53149eb008e0962ad2131
SimaelSouza <simaelsouza2019@gmail.com>
2026-05-17 11:40:15 -0300

    feat(validation): add custom validation messages to DTO attributes

--- src/main/java/com/example/products/dtos/ProductUpdateDto.java
+++ src/main/java/com/example/products/dtos/ProductUpdateDto.java
@@ -7,6 +7,10 @@ import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public record ProductUpdateDto(
-        @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres") String name,
-        @Positive(message = "o valor deve ser maior que 0")@Digits(integer = 10, fraction = 2) BigDecimal price) {
+        @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres")
+        String name,
+
+        @Positive(message = "o valor deve ser maior que 0")
+        @Digits(integer = 10, fraction = 2)
+        BigDecimal price) {
 }
